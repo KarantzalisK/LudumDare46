8240940448ba346219beddeb875c5f980e9a47ac
Karantzalis Konstantinos <kostantinoskaratzalis@gmail.com>
2020-04-20 04:21:42 +0300

    Player Attack

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,50 +5,51 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
 
-    //public float turnSpeed = .1f;
-    //public float moveSpeed = 4f;
 
-
-    //Rigidbody2D target;
-
-    //Rigidbody2D rb;
-
-    //public Vector2 attackOffset;
-
-    //float rotAngle = 0f;
-
+    public int health;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        //rb = GetComponent<Rigidbody2D>();
-
-        //target = GameObject.FindGameObjectWithTag("player").GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (health == 0) Die(); 
     }
 
     private void FixedUpdate()
     {
-        //Vector2 dir = (target.position - (rb.position + attackOffset)).normalized;
 
-        //float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90f;
-        //rotAngle = Mathf.LerpAngle(rotAngle, angle, turnSpeed);
 
-        //Vector2 newDir = Quaternion.AngleAxis(rotAngle + 90, Vector3.forward) * Vector3.right;
-
-        //Vector2 force = newDir * Time.fixedDeltaTime * moveSpeed;
-
-        //rb.AddForce(force);
 
     }
 
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag.Equals("arrow")) {
+            Destroy(collision.gameObject);
+            health--;
+            StartCoroutine("EndCountdown");
 
+        }
+    }
 
+    void Die() { 
+        
+        
+    }
+
+    IEnumerator EndCountdown()
+    {
+        Color cl = gameObject.GetComponent<SpriteRenderer>().color;
+        gameObject.GetComponent<SpriteRenderer>().color = new Color(100, 0, 100);
+
+        yield return new WaitForSeconds(2f);
+        gameObject.GetComponent<SpriteRenderer>().color= cl;
+
+    }
 
 }
