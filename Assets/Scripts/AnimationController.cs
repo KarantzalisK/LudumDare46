1ec279da72515f06778b4e59102b5ea3b4fa4e0f
NahxEsAzul <nasos.karachalios.stagas@gmail.com>
2020-04-20 08:23:43 +0300

    animation progress

---
+++ Assets/Scripts/AnimationController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{ 
+    public Sprite UP,DOWN,LEFT,RIGHT;
+    private SpriteRenderer sb;
+    private Rigidbody2D rb;
+    public Vector2 currentPosition, previousPosition;
+    public bool flip;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        sb= gameObject.GetComponent<SpriteRenderer>();
+        currentPosition = rb.position;
+        previousPosition = rb.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        currentPosition = rb.position;
+        Vector2 direction = previousPosition - currentPosition;
+        if (direction.x < 0){
+            sb.sprite = LEFT;
+            if (flip){
+                sb.flipX = false;
+            }
+        }if (direction.x > 0){
+            sb.sprite = RIGHT;
+            if (flip==false)
+            {
+                sb.flipX = true;
+            }
+        }
+        if (direction.y < 0){
+            sb.sprite = DOWN;
+           if (gameObject.GetComponentInChildren<SpriteRenderer>().flipY == true)
+            {
+                gameObject.GetComponentInChildren<SpriteRenderer>().flipY = false;
+            }
+
+
+        }
+        if (direction.y > 0){
+            sb.sprite = UP;
+            gameObject.GetComponentInChildren<SpriteRenderer>().flipY = true;
+
+        }
+
+    }
+    IEnumerator directionDetect() {
+
+      
+        yield return new WaitForSeconds(0.2f);
+        previousPosition = rb.position;
+
+    }
+
+}
